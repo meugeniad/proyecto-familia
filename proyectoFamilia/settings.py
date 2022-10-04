6933e6b57d3c49dd288b17ebb3a08a3d46e9503d
Maria Eugenia Dearriba <eugenia.dearriba@gurusoluciones.com>
2022-10-04 20:24:39 -0300

    Corregi el problema de setting con BASE_DIR

--- proyectoFamilia/settings.py
+++ proyectoFamilia/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'proyectoFamilia.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [ r'C:\Users\medea\OneDrive\Documentos\Curso Python\Clases y Ejercicios\proyecto-familia\proyectoFamilia\templates'],
+        'DIRS': [ BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
